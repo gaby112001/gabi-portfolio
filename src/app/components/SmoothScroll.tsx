a6e45201de00332ac9aff964a09e2f91aecd0d10
GabyJacome <gaby112001@hotmail.com>
2026-02-17 23:45:07 -0500

    Add smooth scrolling with Lenis

---
+++ src/app/components/SmoothScroll.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+
+export default function SmoothScroll({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+  duration: 1.1,
+  smoothWheel: true,
+});
+
+
+    let raf = 0;
+    const loop = (time: number) => {
+      lenis.raf(time);
+      raf = requestAnimationFrame(loop);
+    };
+    raf = requestAnimationFrame(loop);
+
+    return () => {
+      cancelAnimationFrame(raf);
+      // @ts-ignore
+      lenis.destroy?.();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
